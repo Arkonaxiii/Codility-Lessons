966e33f093f56d531fa7b97df0a59e8b3f22cb65
Arkonaxiii <marta.m.potocka@gmail.com>
2016-05-25 14:20:10 +0200

    Delete Lesson4 PermCheck.py

--- Lesson4 PermCheck.py
+++
@@ -1,9 +0,0 @@
-def solution(A):
-    result = 0
-
-    for index in range(len(A)):
-        result = result ^ A[index] ^ (index + 1)
-    if result == 0:
-        return 1
-    else:
-        return 0
