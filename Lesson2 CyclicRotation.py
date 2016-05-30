5d83d576673bc43a913ba14b75c93862cd4f4784
Arkonaxiii <marta.m.potocka@gmail.com>
2016-05-30 22:21:14 +0200

    Update Lesson2 CyclicRotation.py

--- Lesson2 CyclicRotation.py
+++ Lesson2 CyclicRotation.py
@@ -1,9 +1,5 @@
 def solution(A, K):
     if len(A) == 0:
         return A
-    if K > len(A):
-        shift = K % len(A)
-    else:
-        shift = K
-
+    shift = K % len(A) 
     return A[-shift:] + A[:-shift]
