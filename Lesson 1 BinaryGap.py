7016de213f809a79abb38074bd5134c2aa04d961
Arkonaxiii <marta.m.potocka@gmail.com>
2016-05-30 21:57:39 +0200

    Update Lesson 1 BinaryGap.py

--- Lesson 1 BinaryGap.py
+++ Lesson 1 BinaryGap.py
@@ -1,3 +1,5 @@
+# note that gap is only between 1s so e.g. 11000 has NO gap
+
 def solution(N):
     
     B = bin(N)[2:]
@@ -9,9 +11,7 @@ def solution(N):
         if digit == "1":
             if temp > longest:
                 longest = temp
-            temp = 0
+                temp = 0
         else:
-            temp += 1
-    if temp > longest:
-        longest = temp       
+            temp += 1      
     return longest
