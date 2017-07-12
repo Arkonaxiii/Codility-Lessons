fda5b45fb7edc643afb0ad9b57af45824d87a83a
Arkonaxiii <marta.m.potocka@gmail.com>
2017-07-12 21:36:02 +0200

    Update Lesson3 PermMissingElement Enumerate XOR.py

--- Lesson3 PermMissingElement Enumerate XOR.py
+++ Lesson3 PermMissingElement Enumerate XOR.py
@@ -2,6 +2,5 @@ def solution(A):
     
     xor_sum = len(A)+1
     for index,element in enumerate(A,1):
-        xor_sum ^= index
-        xor_sum ^= element
+        xor_sum = xor_sum ^ index ^ element  # (P ^ Q) ^ R = P ^ (Q ^ R) = P ^ Q ^ R
     return xor_sum
