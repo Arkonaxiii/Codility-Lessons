e1876602e1bab26a758a4711b4973bc11a5e3974
Arkonaxiii <marta.m.potocka@gmail.com>
2017-07-13 23:11:25 +0200

    Update Lesson4 MissingInteger.py

--- Lesson4 MissingInteger.py
+++ Lesson4 MissingInteger.py
@@ -3,8 +3,21 @@ def solution(A):
 
     for element in A:
         if 0 < element <= (len(A) + 1):
-            if table[element - 1] == 0:
+            if table[element - 1] == 0:   # chyba można by bez ifa, i od razu zrobić przypisanie?
                 table[element - 1] = 1
     for index in range(len(A) + 1):
         if table[index] == 0:
             return index + 1
+        
+        
+       albo:
+        
+ def solution(A):
+    D ={}
+    for i in range(len(A)+1):
+        D[i+1] = 0
+    for i in range(len(A)):
+        D[A[i]] = 1
+    for k in D.keys():
+        if D[k] == 0:
+            return k       
