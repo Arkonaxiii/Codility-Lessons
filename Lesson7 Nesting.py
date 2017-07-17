a5ec783041e1970f4819e577d76e3f65462124f4
Arkonaxiii <marta.m.potocka@gmail.com>
2017-07-17 22:30:19 +0200

    Update Lesson7 Nesting.py

--- Lesson7 Nesting.py
+++ Lesson7 Nesting.py
@@ -4,7 +4,7 @@ def solution(S):
     for s in S:
         if s == '(':
             stack.append(s)
-        if s == ')':
+        else:
             if len(stack) == 0:
                 return 0
             stack.pop()
@@ -12,3 +12,22 @@ def solution(S):
         return 1
     else:
         return 0
+    
+    #można bez stosu, sam counter
+    
+    def solution(S):
+    if len(S) == 0:
+        return 1
+    res = 0
+    for p in S:
+        if p == '(':
+            res += 1
+        else:
+            res -= 1
+        if res < 0:
+            return 0
+    if res == 0:
+        return 1
+    else:
+        return 0
+    
